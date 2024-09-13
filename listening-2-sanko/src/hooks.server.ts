2b0bb9ba5d7ef056eefaf9ffb3da18f9650b1c13
SwimmingPigLuvr <dnevell95@gmail.com>
2024-09-12 19:02:38 -0700

    Updated files: listening-2-sanko/src/hooks.server.ts listening-2-sanko/src/lib/server/websocket.ts

--- listening-2-sanko/src/hooks.server.ts
+++ listening-2-sanko/src/hooks.server.ts
@@ -1,17 +1,6 @@
-// src/hooks.server.ts
 import type { Handle } from '@sveltejs/kit';
-import { setupWebSocketServer } from '$lib/server/websocket';
 
 export const handle: Handle = async ({ event, resolve }) => {
-    console.log('src/hooks.server.ts***');
-    // Only try to set up WebSocket server in production
-    if (process.env.NODE_ENV === 'production') {
-        const { server } = event.platform;
-        if (server) {
-            setupWebSocketServer(server);
-        }
-    }
-
     const response = await resolve(event);
     return response;
 };
